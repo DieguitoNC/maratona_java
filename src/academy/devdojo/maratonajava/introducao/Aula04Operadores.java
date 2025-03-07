27946200885b43fb7e6b74dbc01fc5f178d65773
DieguitoNC <diegocatalann@gmail.com>
2025-03-06 21:22:31 -0300

    operador AND

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -22,5 +22,15 @@ public class Aula04Operadores {
         System.out.println("isDezMenorQueVinte "+ isDezMenorQueVinte);
         System.out.println("isDezIgualVinte "+ isDezIgualVinte);
         System.out.println("isDezDiferenteVinte "+ isDezDiferenteVinte);
+
+        // && (AND)  || (OR) !
+
+        int idade = 29;
+        float salario = 3500f;
+        boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario > 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+
+        System.out.println("isDentroDaLeiMaiorQueTrinta "+isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta "+isDentroDaLeiMenorQueTrinta);
     }
 }
